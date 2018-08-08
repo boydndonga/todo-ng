541a2a2544426eec735dae1fdd4a2ff4472d5e3d
Boyd Ndonga <boydbugle@gmail.com>
2018-08-09 01:09:10 +0300

    test creation of todo items

--- src/app/todo-data.service.spec.ts
+++ src/app/todo-data.service.spec.ts
@@ -1,6 +1,6 @@
 import { TestBed, inject } from '@angular/core/testing';
-
 import { TodoDataService } from './todo-data.service';
+import { Todo } from './todo';
 
 describe('TodoDataService', () => {
   beforeEach(() => {
@@ -12,4 +12,35 @@ describe('TodoDataService', () => {
   it('should be created', inject([TodoDataService], (service: TodoDataService) => {
     expect(service).toBeTruthy();
   }));
+
+  describe('#save(todo)', () => {
+
+    it('should automatically assign an incrementing id', inject([TodoDataService], (service: TodoDataService) => {
+      const todo1 = new Todo({title: 'Hello 1', complete: false});
+      const todo2 = new Todo({title: 'Hello 2', complete: true});
+      service.addTodo(todo1);
+      service.addTodo(todo2);
+      expect(service.getTodoById(1)).toEqual(todo1);
+      expect(service.getTodoById(2)).toEqual(todo2);
+    }));
+
+  });
+
+  // describe('#getAllTodos()', () => {
+
+  //   it('should return an empty array by default', inject([TodoDataService], (service: TodoDataService) => {
+  //     expect(service.getAllTodos()).toEqual([]);
+  //   }));
+
+  //   it('should return all todos', inject([TodoDataService], (service: TodoDataService) => {
+  //     const todo1 = new Todo({title: 'Hello 1', complete: false});
+  //     const todo2 = new Todo({title: 'Hello 2', complete: true});
+  //     service.addTodo(todo1);
+  //     service.addTodo(todo2);
+  //     expect(service.getAllTodos()).toEqual([todo1, todo2]);
+  //   }));
+
+  // });
+
+
 });
